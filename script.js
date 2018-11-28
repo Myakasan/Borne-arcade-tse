db561526077477d03d14fa4093323d156fe4f503
Ressort Aloïs <ressort.alois@gmail.com>
2018-11-28 12:24:52 +0100

    Ca commence a ressembler a quelque chose

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ GameCore.prototype.update = function()
 
 GameCore.prototype.trackControls = function()
 {
-    if (gameKeysHeld["w"]) {
+    if (gameKeysHeld["z"]) {
         leftPaddle.move(GameDirection.UP);
     }
 
@@ -91,7 +91,7 @@ GameCore.prototype.drawSplashScreen = function(paddle)
     if(paddle != null)
     {
         var victoryString = (!paddle.isRightPaddle) ? "Player one wins!" : "Player two wins!";
-        this.ctx.fillText(victoryString, 300, 300);
+        this.ctx.fillText(victoryString, 300, 350);
     }
 
     this.ctx.fillText("Please press Spacebar to start.", 5, 250);
